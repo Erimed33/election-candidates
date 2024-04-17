b5a859317cceda5ace20aed46991da2cc90c8e2c
Erimed33 <erikamedina@pursuit.org>
2024-04-17 12:18:30 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import NavBar from "./components/Navbar.jsx";
+import List from "components/List.jsx";
+
 
 function App() {
 
   return (
     <>
-      <div> hey there</div>
+      <div> hey there m</div>
+      <List/>
+      <NavBar/>
     </>
   )
 }
